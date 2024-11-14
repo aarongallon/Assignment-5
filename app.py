7484a783d80f8914850d1eee18a91104911755a8
aarongallon <aarongallon0402@gmail.com>
2024-11-14 11:45:19 -0500

    commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,17 @@ def home():
 @app.route('/tableview')
 def list():
     return render_template('list.html')
+
+def add_baker():
+    if request.method == 'POST':
+        # Retrieve form data
+        name = request.form.get('Name', '').strip()
+        age = request.form.get('Age', '').strip()
+        phone_number = request.form.get('PhoneNumber', '').strip()
+        security_level = request.form.get('SecurityLevel', '').strip()
+        password = request.form.get('Password', '').strip()
+
+    return render_template
 
 if __name__ == '__main__':
     app.run(debug=True)
